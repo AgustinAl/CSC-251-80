7360044016e8cd17caae2dd7dd3e043bf56b43a2
Agustin <agustin434390@gmail.com>
2020-02-24 12:22:35 -0800

    First

--- aalejandre06.c
+++ aalejandre06.c
@@ -6,22 +6,33 @@ Lab 06
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+
 
 int main(void)
 {
-	int x=0,y=0,z=0,i=0;
-//	printf("enter a seed \n");
-//	scanf("%d", &y);
-	srand(time(NULL));
-	
-	for(i=0 ;i<11 ;i++)
+	int firstArray[ 5 ]={ 0, 2, 4, 6, 8 };
+	int secondArray[ 5 ]={ 1, 3, 5, 7, 9 };
+	int temp1;
+	int temp2;
+	int i,j;
+
+	printf("Before Swap \n");
+
+	for(i=0;i<5;i++)
 	{
-		x=(rand()%30);
-		x=x+50;
-		printf("x = %d y= %d z= %d \n",x,y,z);
-	} 
-      
-  	
+		printf("firstArray[%d] = %d, secondArray[%d] = %d \n" , i, firstArray[i], i, secondArray[i]);
+	}
+
+	printf("After Swap \n");
+
+	for(j=0;j<5;j++)
+	{
+		temp1 = firstArray[j];
+		temp2 = secondArray[j];
+		firstArray[j] = temp2;
+		secondArray[j] = temp1;
+		printf("firstArray[%d] = %d, secondArray[%d] = %d \n" , j, firstArray[j], j, secondArray[j]);
+
+	}
 	return EXIT_SUCCESS;
 }
