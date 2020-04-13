a5d1d9942b3fb5a6a9342392d45eeb490e55a1d7
Agustin <agustin434390@gmail.com>
2020-04-13 12:45:10 -0700

    first

---
+++ aalejandre09.c
@@ -0,0 +1,119 @@
+//Name:Agustin Alejandre
+//Class: CSC 251-80
+//Lab 09
+
+#include<stdlib.h>
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+void lowercase(char *r);
+void uppercase(char *r);
+void vowels(char *r);
+int numbers(char *r);
+
+int main(void)
+{
+	int x,y;
+	char charray[20];
+	printf("Enter a string less than 20 characters that includes one number");
+	fgets(charray,20,stdin);
+	char *ptr=charray;
+	lowercase(ptr);
+	uppercase(ptr);
+	vowels(ptr);
+	x=numbers(ptr);
+	y=x*12;
+	printf("(Numbers Entered is String) %d * 12 = %d \n",x,y);
+	return EXIT_SUCCESS;
+}
+void lowercase(char *r)
+{
+	int i;
+	printf("Lowercase Letters: \n");
+	for(i=0;i<20;i++)
+	{
+		if(islower(*r)>0)
+		{
+			printf("%c",*r);
+		}
+		r++;
+	}
+	
+	printf("\n");
+}
+void uppercase(char *r)
+{
+	int i;
+	printf("Uppercase Letters: \n");
+	for(i=0;i<20;i++)
+	{
+		if(isupper(*r)>0)
+		{
+			printf("%c",*r);
+		}
+		r++;
+	}
+	printf("\n");
+}
+void vowels(char *r)
+{
+	int i;
+	int x=0;
+	printf("Number of Vowels \n");
+	for(i=0;i<20;i++)
+	{
+		if(*r == 'a' || *r == 'e' || *r == 'i' || *r == 'o' || *r == 'u' || *r == 'y' || *r == 'A' || *r == 'E' || *r == 'I' || *r == 'O' || *r == 'U' || *r == 'Y')
+		{
+			x++;
+		}
+	r++;
+	}
+	printf("%d \n",x);
+}
+int numbers(char *r)
+{
+	int i;
+	int x;
+	for(i=0;i<20;i++)
+	{
+		if(isdigit(*r)>0)
+		{
+			x = *r-'0';
+		}
+		r++;
+	}
+	return x;
+}
+/*
+
+	if(0 == strcmp(charray,charra))
+	{
+		puts("that is the correct");
+	}
+	else
+	{
+		puts("wrong");
+	}
+
+	printf("enter string ");
+	while(ch != '\n')
+	{
+		ch = getchar();
+
+		if(isdigit(ch))
+		{
+			puts("is digit ");
+		}
+		else if(islower(ch))
+		{
+			ch = toupper(ch);
+			printf("Your lower char is now upper -> %c \n ",ch);
+		}
+		else
+		{
+			printf(" all else is = %c \n ",ch);
+		}
+	}
+*/
+	
